46b41fbd81a33d25689bab331fcb0a943b6a5323
norbertastier <astier.norbert@gmail.com>
2023-02-12 22:39:22 +0100

    speech test

--- tools/sentiment_analysis.py
+++
@@ -1,9 +0,0 @@
-from transformers import AutoTokenizer, TFAutoModelForSequenceClassification
-from transformers import pipeline
-
-tokenizer = AutoTokenizer.from_pretrained("tblard/tf-allocine")
-model = TFAutoModelForSequenceClassification.from_pretrained("tblard/tf-allocine")
-
-nlp = pipeline('sentiment-analysis', model=model, tokenizer=tokenizer)
-
-print(nlp("c'est bien."))
